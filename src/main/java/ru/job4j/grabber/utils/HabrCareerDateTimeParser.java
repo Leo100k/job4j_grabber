dc0d667c0b122374becb95661a299fdd65af054c
Leo100k <lemail2004@mail.ru>
2024-01-28 11:41:39 +0300

    2.1. Преобразование даты [#289476 #476970]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -7,15 +7,12 @@ public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        String strParse = parse.substring(0, 19);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-        return LocalDateTime.parse(strParse, formatter);
+        return LocalDateTime.parse(parse, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
     }
 
     public static void main(String[] args) {
         HabrCareerDateTimeParser dateToLD = new HabrCareerDateTimeParser();
-        LocalDateTime datePrint = dateToLD.parse("2024-01-23T12:44:00+03:00");
+        LocalDateTime datePrint = dateToLD.parse("2024-01-23T12:44:01+03:00");
         System.out.println(datePrint);
-
     }
 }
